c856b1d21e878df0660244c11134c163a74f4beb
kdmcnamara <kdmcnamara@wisc.edu>
2018-06-27 13:32:01 -0500

    https ajax link

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ app.use('/', express.static('public'));
 
 
 
-var server = http.createServer(app);
+
+
 
 app.listen(process.env.PORT || 3000, () =>  {
 	console.log("Server is running on port 3000!");
