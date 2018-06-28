bb9f503b2ec8159e755ca2555bd87b39c530fa5f
kdmcnamara <kdmcnamara@wisc.edu>
2018-06-28 15:55:37 -0500

    Test Jenkins CI

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ var server = supertest.agent("http://localhost:3000");
 describe("SAMPLE unit test", function() {
 
   // #1 should return home page
-  it("should return home page", function(done) {
+  it("Should return home page", function(done) {
     server
     .get("/")
     .expect("Content-type", /text/)
@@ -20,5 +20,5 @@ describe("SAMPLE unit test", function() {
       done();
     });
   });
-  
+
 });
